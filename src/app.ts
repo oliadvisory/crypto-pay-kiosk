d1ea0b71d38ed01313e6fcabffb28e1133ff3594
Chase Oliphant <chaseoliphant@gmail.com>
2021-06-08 17:59:41 -0700

    build tsoa

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { RegisterRoutes } from "../build/routes";
+import { RegisterRoutes } from "./routes";
 
 export const app = express();
 
